ed5607768fd56e95af74a52cb579b549bae2b6b9
Melissa <melissa.aguiar@engenharia.ufjf.br>
2020-06-22 16:23:03 -0300

    Clones do projeto SSF

---
+++ ssf/ssf - Copia (8)/Hardware/Proc_IP/positivo_fx.v
@@ -0,0 +1,29 @@
+module positivo_fx
+#(
+	parameter NUBITS = 32,
+	parameter [NUBITS-1:0] NUGAIN = 128
+)
+
+(
+	input  [NUBITS-1:0] acc,
+	input 		   neg,norm,
+	output [NUBITS-1:0] out
+);
+
+reg  [NUBITS-1:0] x;
+
+
+always @(*) begin
+
+	if ((acc[NUBITS-1] == 1) && (neg ==1))
+		x <= 0;	
+	else
+		if(norm ==1)
+			x <= acc/NUGAIN;
+		else
+			x <= acc;	
+end
+
+assign out = x;
+
+endmodule
